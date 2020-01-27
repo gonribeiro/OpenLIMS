e70ca1d9e7ddd0b70ee740d1b5bd4ab5dd8e2f4c
Gon <gon@gon>
2020-01-26 22:48:19 -0300

    Projeto Inicial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Erro 404 
+          <br/ ><code>Página não encontrada</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
